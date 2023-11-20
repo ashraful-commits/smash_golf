fad9149f0c40ff0782d0aa433293f23a71eef542
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-21 00:05:30 +0600

    fist comment

---
+++ components/MaxWidthContainer.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const MaxWidthContainer = () => {
+  return (
+    <div className={cn("min-w-[1440px] flex justify-center")}>
+      MaxWidthContainer
+    </div>
+  );
+};
+
+export default MaxWidthContainer;
