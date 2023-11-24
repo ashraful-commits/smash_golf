6de0c04fce9c3308fe4fbf3a9cbd1403445262af
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-25 04:09:21 +0600

    updated

--- Utility/Login.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const LoginUser = async (data: any) => {
-  const response = await axios.post("http://localhost:3000/api/login", data);
-  console.log(response);
-  return response;
-};
