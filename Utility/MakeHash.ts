ad34690b0e4346aba6d2098f5ea488f2f7feacdd
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-25 02:24:35 +0600

    upated

--- Utility/MakeHash.ts
+++
@@ -1,6 +0,0 @@
-import bcryptjs from "bcryptjs";
-export const makeHash = async (password: any) => {
-  const salt = await bcryptjs.genSaltSync(10);
-  const hashPass = bcryptjs.hash(password, salt);
-  return hashPass;
-};
--- Utility/MakeToken.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-export const makeToken = (
-  data: any,
-  jwtF = process.env.JWT_SECRECT,
-  exp = "7d"
-) => {
-  const jwtToken = jwt.sign(data, jwtF, { expiresIn: exp });
-  return jwtToken;
-};
