526a5c970812cbbf5e6c9e2c34b578af8ad9ccdf
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-25 02:56:09 +0600

    upated

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   MongoDbConection();
-  let UserModel:any
+  let UserModel: any;
   const data = await UserModel.find();
   return NextResponse.json({ message: data });
 }
