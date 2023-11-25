461cc8f95532cd4da027bb3cb2c6384976c3684d
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-26 01:58:57 +0600

    updated

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import React, { useState } from "react";
-import { render } from "react-dom";
+
 const Login = (props: any) => {
   const router = useRouter();
   const [input, setInput] = useState({
