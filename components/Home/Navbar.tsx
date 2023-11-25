2dd552c12321b475a781229c7d8bb691735731af
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-26 00:27:40 +0600

    updated

--- components/Home/Navbar.tsx
+++ components/Home/Navbar.tsx
@@ -8,7 +8,6 @@ import NavItem from "./NavItem";
 import Link from "next/link";
 
 const Navbar = ({ session }: any) => {
-
   const NavItems = [
     {
       path: "/",
@@ -57,11 +56,11 @@ const Navbar = ({ session }: any) => {
   useEffect(() => {
     if (scrollY > 0) {
       setStickyHeader(0);
-    }else{
-      setStickyHeader(35)
+    } else {
+      setStickyHeader(35);
     }
   }, [scrollY]);
-console.log(stickyHeader)
+  console.log(stickyHeader);
   return (
     <MaxWidthContainer
       className={`w-[1440px] transition-all sticky duration-500 ease-in-out  z-[9999999] px-[100px] m-auto top-[${stickyHeader}px] left-0  h-[98px] min-w-[1440px] ${
