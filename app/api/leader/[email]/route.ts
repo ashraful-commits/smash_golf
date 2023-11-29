2cef7996b773531488c98fbf734f32401dd0fc7e
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-30 03:10:05 +0600

    updated

--- app/api/leader/[email]/route.ts
+++ app/api/leader/[email]/route.ts
@@ -2,20 +2,19 @@ import { MongoDbConection } from "@/config/MongodbConection";
 import UserModel from "@/model/User";
 import { NextResponse } from "next/server";
 
-    export async function GET(req: Request, { params }) {
-        const email = params.email;
-        MongoDbConection();
-        const singleData = await UserModel.findOne({ email });
-        return NextResponse.json({ user: singleData });
-      }
+export async function GET(req: Request, { params }: any) {
+  const email = params.email;
+  MongoDbConection();
+  const singleData = await UserModel.findOne({ email });
+  return NextResponse.json({ user: singleData });
+}
 
- 
 export async function HEAD(request: Request) {}
- 
+
 export async function POST(request: Request) {}
- 
+
 export async function PUT(request: Request) {}
- 
+
 export async function DELETE(request: Request) {}
- 
+
 export async function PATCH(request: Request) {}
