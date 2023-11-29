8b7bbc6244878b23b359dc111c6bb4f54850e672
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-30 02:13:54 +0600

    updated

--- app/user/[email]/page.tsx
+++ app/user/[email]/page.tsx
@@ -2,13 +2,9 @@ import MaxWidthContainer from "@/components/user/MaxWidthContainer";
 import Image from "next/image";
 import Header from "@/components/user/Header";
 import MainContainer from "@/components/user/MainContainer";
-import { getServerSession } from "next-auth";
-import { getSingleUser } from "@/Utility/GetSingleUser";
 
 const User = async () => {
-  const session = await getServerSession();
 
-  const userData = await getSingleUser({ email: session?.user?.email });
 
   return (
     <MaxWidthContainer>
